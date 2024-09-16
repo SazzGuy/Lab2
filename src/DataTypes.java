2e5e2c0fedafbb2406ece0eaeff3aa6e73c7c3d2
SazzGuy <saswatamoh@gmail.com>
2024-09-16 18:09:55 -0400

    fixed error in dataypes.java

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -4,10 +4,10 @@ public class DataTypes {
     // TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0;
         // below is a "foreach" loop which iterates through numbers
-        for (int i = 0; i < numbers.size(); i++) {
-            s += i;
+        for (int x: numbers) {
+            s += x;
         }
         return s;
     }
